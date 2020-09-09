419cb2e5313c396d47ae2068dbf3857256467e98
johnwall <1047994907@qq.com>
2020-09-09 16:11:25 +0800

    异常处理

--- nettydemo/src/main/java/com/shulian/netty/handler/DiscardOutboundHandler.java
+++ nettydemo/src/main/java/com/shulian/netty/handler/DiscardOutboundHandler.java
@@ -1,10 +1,8 @@
 package com.shulian.netty.handler;
 
-import io.netty.channel.ChannelHandler;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelOutboundHandlerAdapter;
-import io.netty.channel.ChannelPromise;
+import io.netty.channel.*;
 import io.netty.util.ReferenceCountUtil;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * On the outbound side, if you handle a write() operation and discard a message,
@@ -15,15 +13,17 @@ import io.netty.util.ReferenceCountUtil;
  * @date 2020-09-08 18:30
  * @since jdk1.8
  */
+@Slf4j
 @ChannelHandler.Sharable
 public class DiscardOutboundHandler extends ChannelOutboundHandlerAdapter {
     /**
      * It’s important not only to release resources but also to notify the ChannelPromise.
      * Otherwise a situation might arise where a ChannelFutureListener has not been notified about a message that has been handled.
-     *  In sum, it is the responsibility of the user to call ReferenceCountUtil.release() if
+     * In sum, it is the responsibility of the user to call ReferenceCountUtil.release() if
      * a message is consumed or discarded and not passed to the next ChannelOutboundHandler in the ChannelPipeline.
      * If the message reaches the actual transport layer, it
      * will be released automatically when it’s written or the Channel is closed
+     *
      * @param ctx
      * @param msg
      * @param promise
@@ -31,7 +31,33 @@ public class DiscardOutboundHandler extends ChannelOutboundHandlerAdapter {
      */
     @Override
     public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
-        ctx.write(msg);
+        ChannelFuture future = ctx.write(msg);
+        //处理 outbound 事件异常，可以通过注册监听器到future里面，outboundHandler的几乎所有方法都带有一个参数 channelpromise,
+        // channelpromise 是 ChannelFuture 的子接口
+        future.addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture future) throws Exception {
+                if (!future.isSuccess()) {
+                    Throwable cause = future.cause();
+                    log.warn("DiscardOutboundHandler捕获异常", cause);
+                    future.channel().close();
+                }
+            }
+        });
+
+        //The second option is to add a ChannelFutureListener to the ChannelPromise that is
+        //passed as an argument to the ChannelOutboundHandler methods.  和 上面 把监听器注册到 future  有相同的效果
+        promise.addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture future) throws Exception {
+                if (!future.isSuccess()) {
+                    Throwable cause = future.cause();
+                    log.warn("DiscardOutboundHandler捕获异常", cause);
+                    future.channel().close();
+                }
+            }
+        });
+
         ReferenceCountUtil.release(msg);
 //        Notifies ChannelPromise that data was handled
         promise.setSuccess();
--- nettydemo/src/main/java/com/shulian/netty/handler/SharableHandler.java
+++ nettydemo/src/main/java/com/shulian/netty/handler/SharableHandler.java
@@ -3,16 +3,19 @@ package com.shulian.netty.handler;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 线程安全的 handler 可以做多个pipeline里面共享，通过打上注解 Sharable。但是还需要保证这样的
  * handler 是无状态的。
+ *
  * @author zhangjuwa
  * @apiNote
  * @date 2020-09-09 12:00
  * @since jdk1.8
  */
 @ChannelHandler.Sharable
+@Slf4j
 public class SharableHandler extends ChannelInboundHandlerAdapter {
 
 
@@ -22,7 +25,7 @@ public class SharableHandler extends ChannelInboundHandlerAdapter {
      * the ChannelPipeline will very likely produce errors when it’s accessed by concurrent
      * channels. (Of course, this simple case could be corrected by making channelRead()
      * synchronized.)
-     *  In summary, use @Sharable only if you’re certain that your ChannelHandler is
+     * In summary, use @Sharable only if you’re certain that your ChannelHandler is
      * thread-safe.
      */
     private int count;
@@ -34,4 +37,13 @@ public class SharableHandler extends ChannelInboundHandlerAdapter {
 //        ctx.name()
 
     }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        //处理 inbound 事件的异常，典型的方式是重写  exceptionCaught 方法，如果所有的inboundHandler
+//        都不处理异常，到了pipeline尾部，会打印异常日志并标记这个异常未做处理
+        log.warn("DiscardOutboundHandler捕获异常", cause);
+        ctx.close();
+    }
+
 }
