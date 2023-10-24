368fbb7e17c11766314cfaf1c286c0e96a66e403
zhangjuwa <1047994907@qq.com>
2023-10-25 00:45:59 +0800

    flink学习 day6 ：Anatomy of a Flink Program 简单dataStream数据处理   source(file,collection...)->(map,filter,group) -> sink(file,stdout...)

--- flink/src/main/java/com/john/flink/StreamSourceDemo.java
+++ flink/src/main/java/com/john/flink/StreamSourceDemo.java
@@ -1,5 +1,6 @@
 package com.john.flink;
 
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.connector.file.src.FileSource;
 import org.apache.flink.connector.file.src.reader.TextLineInputFormat;
 import org.apache.flink.core.fs.Path;
@@ -19,7 +20,7 @@ import java.util.List;
 public class StreamSourceDemo {
 
     @Test
-    public void fileSource() {
+    public void collectionSource() {
         List<Person> people = new ArrayList<Person>();
 
         people.add(new Person("Fred", 35));
@@ -36,6 +37,52 @@ public class StreamSourceDemo {
         // or a file
 //        environment.readTextFile()
 
+
+    }
+
+    @Test
+    public void fileSource() {
+        Path path = new Path("/Users/zhangjuwa/Documents/javaProject/springcloud/xxx.csv");
+//        final FileSource<String> source =
+//                FileSource.forRecordStreamFormat(new PojoCsvInputFormat<Person>(path, null))
+//        .monitorContinuously(Duration.ofMillis(5))
+//                .build();
+    }
+
+    /**
+     * 在此示例中，我们创建了一个 DataStream，其中包含作为字符串的文本文件的行。 此处不需要水印策略，因为记录不包含事件时间戳。
+     */
+    @Test
+    public void readTextFile() throws Exception {
+
+        // 从文件流中读取文件内容
+//        FileSource.forRecordStreamFormat(StreamFormat,Path...);
+
+// 从文件中一次读取一批记录
+//        FileSource.forBulkFileFormat(BulkFormat,Path...);
+
+        StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
+        TextLineInputFormat inputFormat = new TextLineInputFormat();
+        Path path = new Path("/Users/zhangjuwa/Documents/javaProject/springcloud/README.md");
+        FileSource<String> source = FileSource.forRecordStreamFormat(inputFormat, path).build();
+        DataStreamSource<String> fileDataSource = environment.fromSource(source, WatermarkStrategy.noWatermarks(),
+                "README.md");
+        // 使用标准输出sink PrintSinkFunction
+//        fileDataSource.print();
+
+        // 先 得到每行字符的 长度 ，再打印  PrintSinkFunction
+//        fileDataSource.map((MapFunction<String, Integer>) String::length)
+//                .print();
+
+        // 写到一个文件下面  todo
+//        fileDataSource.addSink(new FileSink())
+
+        System.out.println("开始执行");
+        environment.execute();
+    }
+
+    @Test
+    public void readCsvFile() {
 
     }
 
