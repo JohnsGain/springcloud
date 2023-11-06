6a421683255fbb7c6910f5e9dad1a031abde461c
zhangjuwa <1047994907@qq.com>
2023-11-07 01:22:34 +0800

    flink学习 day11 ：Data Pipelines & ETL- stateless trans- flatmap keyBy maxBy aggregation

--- flink/src/main/java/com/john/flink/demo/StatelessTransformationDemo.java
+++ flink/src/main/java/com/john/flink/demo/StatelessTransformationDemo.java
@@ -8,10 +8,18 @@ import com.john.flink.ridecleanse.solution.RideCleansingSolution;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+import org.joda.time.Duration;
+import org.joda.time.Minutes;
 import org.junit.Test;
 
 /**
@@ -38,6 +46,66 @@ public class StatelessTransformationDemo {
 
         System.out.println("开始执行");
         environment.execute();
+    }
+
+    @Test
+    public void flatmap() throws Exception {
+        StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
+        DataStreamSource<TaxiRide> addedSource = environment.addSource(new TaxiRideGenerator());
+        SingleOutputStreamOperator<EnrichedRideDemo> flatted = addedSource.flatMap(new NYCEnrichment());
+        flatted.print();
+
+        environment.execute();
+    }
+
+    @Test
+    public void keyBy1() throws Exception {
+        StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
+        DataStreamSource<TaxiRide> addSource = environment.addSource(new TaxiRideGenerator());
+        SingleOutputStreamOperator<EnrichedRideDemo> flattedMap = addSource.flatMap(new NYCEnrichment());
+
+        KeyedStream<EnrichedRideDemo, Integer> keyedBy = flattedMap.keyBy((KeySelector<EnrichedRideDemo, Integer>) EnrichedRideDemo::getStartCell);
+
+    }
+
+    @Test
+    public void keyBy2() throws Exception {
+        StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
+        DataStreamSource<TaxiRide> source = environment.addSource(new TaxiRideGenerator());
+        SingleOutputStreamOperator<EnrichedRideDemo> flatMap = source.flatMap(new NYCEnrichment());
+        KeyedStream<EnrichedRideDemo, Integer> keyed = flatMap.keyBy(item -> GeoUtils.mapToGridCell(item.getStartLon(), item.getStartLat()));
+
+    }
+
+    @Test
+    public void aggregation() throws Exception {
+        StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
+        DataStreamSource<TaxiRide> addedSource = environment.addSource(new TaxiRideGenerator());
+        SingleOutputStreamOperator<EnrichedRideDemo> streamOperator = addedSource.map(new Enrichment());
+
+        SingleOutputStreamOperator<Tuple2<Integer, Minutes>> flattedMap = streamOperator.flatMap((FlatMapFunction<EnrichedRideDemo, Tuple2<Integer, Minutes>>) (value, out) -> {
+            if (!value.isStart) {
+
+                out.collect(new Tuple2<>(value.startCell, Duration.millis(100L).toStandardMinutes()));
+            }
+        });
+
+        flattedMap.keyBy(item -> item.f0)
+                .maxBy(1)
+                .print();
+
+    }
+
+
+    public static class NYCEnrichment implements FlatMapFunction<TaxiRide, EnrichedRideDemo> {
+
+        @Override
+        public void flatMap(TaxiRide value, Collector<EnrichedRideDemo> out) throws Exception {
+            FilterFunction<TaxiRide> function = new RideCleansingSolution.NYCFilter();
+            if (function.filter(value)) {
+                out.collect(new EnrichedRideDemo());
+            }
+        }
     }
 
     public static class Enrichment implements MapFunction<TaxiRide, EnrichedRideDemo> {
