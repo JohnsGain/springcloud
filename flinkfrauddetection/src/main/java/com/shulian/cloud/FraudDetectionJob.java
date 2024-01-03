427a89a9bf6a999afbf745fef64248ac84d87bc7
zhangjuwa <1047994907@qq.com>
2024-01-03 22:42:20 +0800

    flink学习 day27 ：把flink打成可执行Jar包，上传到 session cluster 模式的flink集群里面执行。 flink的两种集群模式： application cluster, session cluster

---
+++ flinkfrauddetection/src/main/java/com/shulian/cloud/FraudDetectionJob.java
@@ -0,0 +1,33 @@
+package com.shulian.cloud;
+
+import lombok.SneakyThrows;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.walkthrough.common.entity.Alert;
+import org.apache.flink.walkthrough.common.entity.Transaction;
+import org.apache.flink.walkthrough.common.source.TransactionSource;
+
+/**
+ * @author zhangjuwa
+ * @apiNote
+ * @date 2024-01-02 21:36
+ * @since jdk17
+ */
+public class FraudDetectionJob {
+
+    @SneakyThrows
+    public static void main(String[] args) {
+        StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
+        SingleOutputStreamOperator<Transaction> transactions = environment.addSource(new TransactionSource())
+                .name("transactions");
+
+        SingleOutputStreamOperator<Alert> alerts = transactions.keyBy(Transaction::getAccountId)
+                .process(new FraudDetector())
+                .name("fraud-detector");
+
+        alerts.print()
+                .name("send-alerts");
+
+        environment.execute("Fraud Detection");
+    }
+}
